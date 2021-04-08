1e7f7e313d908654f2e1de3eab1d5c3629e6a403
nikita <nikita@ebiworks.con>
2021-04-08 17:49:45 +0300

    Make the initial project setup

---
+++ src/lab_01.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "vector/vector.h"
+#include "stack/stack.h"
+#include "queue/queue.h"
+
+int main(int argc, char** argv)
+{
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
