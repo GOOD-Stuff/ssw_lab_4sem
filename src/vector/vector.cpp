6699307a229d43e618fe41561818bdf4c1d35d8f
Shelbi-Maker <82089215+Shelbi-Maker@users.noreply.github.com>
2021-04-15 20:34:15 +0300

    Vector is implemented

--- src/vector/vector.cpp
+++ src/vector/vector.cpp
@@ -1,1 +1,192 @@
-#include "vector.h"
+#include "Vector.h"
+
+Vector::Vector() {
+	arr = nullptr;
+	size = 0;
+}
+
+Vector::Vector(const Vector& vector)
+{
+	*this = vector;
+}
+
+Vector::~Vector() {
+	if (arr)
+		delete[] arr;
+}
+void Vector::push_back(int value)
+{
+	insert(value, size);
+}
+void Vector::push_front(int value)
+{
+	insert(value, 0);
+}
+
+void Vector::insert(int value, int index )
+{
+	int* arr_new = new int[++size];
+	
+	for (int i = 0; i < index ; i++)
+	{
+		arr_new[i] = arr[i];
+	}
+	
+	arr_new[index] = value;
+
+	for (int i = index + 1; i < size; i++)
+	{
+		arr_new[i] = arr[i - 1];
+
+	}
+
+	delete[] arr;
+	arr = arr_new;
+}
+
+std::ostream& operator<<(std::ostream& stream, const Vector& vector)
+{
+	for (int i = 0; i < vector.size; i++) 
+	{
+		stream << vector.arr[i] << "\n";
+	}
+
+	return stream;
+}
+
+void Vector::pop_back()
+{
+	erase(size - 1);
+}
+
+void Vector::pop_front()
+{
+	erase(0);
+}
+
+void Vector::erase(int index)
+{
+	int* arr_new = new int[size - 1];
+	int t = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (i != index)
+			arr_new[t++] = arr[i];
+
+	}
+	delete[] arr;
+	arr = arr_new;
+	size--;
+}
+
+int Vector::count()
+{
+	return size;
+}
+
+int& Vector::at(int index)
+{
+	return arr[index];
+}
+
+float Vector::get_arithmetic_mean()
+{
+	float sum = 0.0f;
+
+	for (int i = 0; i < size; i++)
+	{
+		sum += arr[i];
+	}
+
+	return sum / size;
+}
+
+Vector Vector::concat_sort(const Vector& vector)
+{
+	Vector new_vector;
+	
+	new_vector.size = size + vector.size;
+	new_vector.arr = new int[new_vector.size];
+
+	int j = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		new_vector.arr[j++] = arr[i];
+	}
+
+	for (int i = 0; i < vector.size; i++)
+	{
+		new_vector.arr[j++] = vector.arr[i];
+	}
+
+	for (int i = 0; i < new_vector.size; i++) {
+		for (int j = 0; j < new_vector.size - 1; j++) {
+			if (new_vector.arr[j] > new_vector.arr[j + 1]) {
+				int temp = new_vector.arr[j];
+				new_vector.arr[j] = new_vector.arr[j + 1];
+				new_vector.arr[j + 1] = temp;
+			}
+		}
+	}
+
+	return new_vector;
+}
+
+Vector& Vector::operator = (const Vector& vector)
+{
+	if (arr)
+		delete[] arr;
+
+	size = vector.size;
+	arr = new int[size];
+
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = vector.arr[i];
+	}
+
+	return *this;
+}
+
+bool Vector::operator ==  (const Vector& right)
+{
+	if (size != right.size)
+		return false;
+	
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] != right.arr[i])
+			return false;
+	}
+
+	return true;
+}
+bool Vector::operator != ( const Vector& right)
+{
+	return !(*this == right);
+}
+bool Vector::operator < ( const Vector& right) 
+{
+	
+	int res_size = (size < right.size) ? size : right.size;
+	for (int i = 0; i < res_size; i++)
+	{
+		if (arr[i] >= right.arr[i])
+			return false;
+	}
+	return true;
+}
+bool Vector::operator > ( const Vector& right)
+{
+	return !(*this < right) && *this != right;
+}
+bool Vector::operator <= ( const Vector& right) 
+{
+	return (*this == right) || (*this < right);
+}
+bool Vector::operator >= ( const Vector& right)
+{
+	return (*this > right) || (*this == right);
+}
--- src/vector/vector.h
+++ src/vector/vector.h
@@ -1,4 +1,43 @@
-class vector {
-    public:
-        int number;
+#ifndef CLASS_VECTOR_H
+#define CLASS_VECTOR_H
+
+#include <ostream>
+
+class Vector
+{
+private:
+	int* arr;
+	int size;
+public:
+	Vector();
+	Vector(const Vector& vector);
+	~Vector();
+
+	void push_back(int value);
+	void push_front(int value);
+	void insert(int value, int index);
+
+	void pop_back();
+	void pop_front();
+	void erase(int index);
+
+	int count();
+
+	int& at(int index);
+	float get_arithmetic_mean();
+
+	Vector concat_sort(const Vector& vector);
+	Vector& operator = (const Vector& right);
+
+	bool operator == (const Vector& right);
+	bool operator != (const Vector& right);
+	bool operator < (const Vector& right);
+	bool operator > (const Vector& right);
+	bool operator <= (const Vector& right);
+	bool operator >= (const Vector& right);
+	friend std::ostream& operator<<(std::ostream& stream, const Vector& vector);
 };
+
+#endif
+
+
