1b91c24a201e3b79b71b0295f7aa3763935e8245
Shelbi-Maker <82089215+Shelbi-Maker@users.noreply.github.com>
2021-04-20 22:34:18 +0300

    Changes to Vector.h

--- src/vector/vector.h
+++ src/vector/vector.h
@@ -6,28 +6,31 @@
 class Vector
 {
 private:
-	int* arr;
-	int size;
+	int* arr;		// Pointer to an array with vector data
+	int size;       // Number of elements in a vector
+
 public:
 	Vector();
 	Vector(const Vector& vector);
 	~Vector();
 
-	void push_back(int value);
-	void push_front(int value);
-	void insert(int value, int index);
+	void push_back(int value);					// Add element to end
+	void push_front(int value);					// Add element to front
+	void insert(int value, int index);			// Insert element by index
 
-	void pop_back();
-	void pop_front();
-	void erase(int index);
+	void pop_back();							// Delete first element
+	void pop_front();							// Delete last element
+	void erase(int index);						// Delete an element by index
 
-	int count();
+	int count();								// Returns the number of elements
 
-	int& at(int index);
-	float get_arithmetic_mean();
+	int& at(int index);							// Returns a reference to an element with a specified index
+	float get_arithmetic_mean();				// Getting the arithmetic average
 
-	Vector concat_sort(const Vector& vector);
+	Vector concat_sort(const Vector& vector);	// Outputs a new collection, linked to another, in a sorted form
 	Vector& operator = (const Vector& right);
+
+	// The lexicographic comparison operators
 
 	bool operator == (const Vector& right);
 	bool operator != (const Vector& right);
@@ -35,7 +38,8 @@ public:
 	bool operator > (const Vector& right);
 	bool operator <= (const Vector& right);
 	bool operator >= (const Vector& right);
-	friend std::ostream& operator<<(std::ostream& stream, const Vector& vector);
+	friend std::ostream& operator<<(std::ostream& stream, const Vector& vector);	// Operator outputs all elements of a vector
+
 };
 
 #endif
